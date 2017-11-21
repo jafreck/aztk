9d63daf682dde9ead0596b8a5ef4ccae02937bc2
Jake Freck <jafreck@microsoft.com>
2017-11-21 10:40:07 -0800

    add Dockerfile and example

---
+++ examples/dask/dask_example.py
@@ -0,0 +1,16 @@
+from dask import delayed
+
+data = [1, 2, 3, 4, 5, 6, 7, 8]
+results = []
+
+def inc(x):
+    return x+1
+
+
+for x in data:
+    y = delayed(inc)(x)
+    results.append(y)
+
+total = delayed(sum)(results)
+
+total.compute()
