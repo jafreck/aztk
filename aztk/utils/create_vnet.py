81670266990984c55715c436be30d21174b579eb
Jake Freck <jafreck@microsoft.com>
2018-01-02 15:24:53 -0800

    fix virtualnetwork and subnet creation

--- aztk/utils/create_vnet.py
+++ aztk/utils/create_vnet.py
@@ -1,5 +1,8 @@
-import aztk.utils.azure_api as azure_api
 import azure.mgmt.network.models as network_models
+from azure.mgmt.batch import BatchManagementClient
+from azure.common.credentials import ServicePrincipalCredentials
+
+import aztk.utils.azure_api as azure_api
 from aztk.utils import constants
 
 
@@ -16,12 +19,25 @@ def create_vnet(tenant_id, client_id, credential, resource_id, vnet_id="aztk-def
 
     match = constants.RESOURCE_ID_PATTERN.match(resource_id)
 
+    credentials = ServicePrincipalCredentials(
+            client_id=client_id,
+            secret=credential,
+            tenant=tenant_id,
+            resource='https://management.core.windows.net/')
+    batch_mgmt_client = BatchManagementClient(credentials, match.group('subscription'))
+    location = batch_mgmt_client.batch_account.get(match.group("resourcegroup"), match.group('account')).location
+
     network_client.virtual_networks.create_or_update(
-        resource_group_name=match.group("resourcegroup"), virtual_network_name=vnet_id, parameters=network_models.VirtualNetwork()
+        resource_group_name=match.group("resourcegroup"),
+        virtual_network_name=vnet_id,
+        parameters=network_models.VirtualNetwork(
+            location=location,
+            address_space=network_models.AddressSpace(["10.0.0.0/24"])
+        )
     )
     
     network_client.subnets.create_or_update(
         resource_group_name=match.group("resourcegroup"),
         virtual_network_name=vnet_id,
         subnet_name=subnet_id,
-        subnet_parameters=network_models.Subnet())
+        subnet_parameters=network_models.Subnet(address_prefix='10.0.0.0/24'))
