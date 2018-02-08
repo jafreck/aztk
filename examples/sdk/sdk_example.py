f6672cd800fa77cf0be9f30b9672e9ac49e7a1dd
Jake Freck <jafreck@microsoft.com>
2018-02-07 17:34:30 -0800

    start fix sdk

--- examples/sdk/sdk_example.py
+++ examples/sdk/sdk_example.py
@@ -1,11 +1,10 @@
 import sys, os, time
-import aztk.models
 import aztk.spark
 from aztk.error import AztkError
 
 # set your secrets
 secrets_confg = aztk.spark.models.SecretsConfiguration(
-    service_principal=aztk.models.ServicePrincipalConfiguration(
+    service_principal=aztk.spark.models.ServicePrincipalConfiguration(
         tenant_id="<org>.onmicrosoft.com",
         client_id="",
         credential="",
@@ -33,7 +32,8 @@ custom_script = aztk.spark.models.CustomScript(
 # define spark configuration
 spark_conf = aztk.spark.models.SparkConfiguration(
     spark_defaults_conf=os.path.join(ROOT_PATH, 'config', 'spark-defaults.conf'),
-    spark_env_sh=os.path.join(ROOT_PATH, 'config', 'spark-env.sh')
+    spark_env_sh=os.path.join(ROOT_PATH, 'config', 'spark-env.sh'),
+    core_site_xml=os.path.join(ROOT_PATH, 'config', 'core-site.xml')
 )
 
 # configure my cluster
@@ -60,19 +60,19 @@ cluster = client.get_cluster(cluster_config.cluster_id)
 client.create_user(cluster.id, "sdk_example_user", "example_password")
 
 # create some apps to run
-app1 = aztk.spark.models.Application(
+app1 = aztk.spark.models.ApplicationConfiguration(
     name="pipy1",
     application=os.path.join(ROOT_PATH, 'examples', 'src', 'main', 'python', 'pi.py'),
     application_args="10"
 )
 
-app2 = aztk.spark.models.Application(
+app2 = aztk.spark.models.ApplicationConfiguration(
     name="pipy2",
     application=os.path.join(ROOT_PATH, 'examples', 'src', 'main', 'python', 'pi.py'),
     application_args="20"
 )
 
-app3 = aztk.spark.models.Application(
+app3 = aztk.spark.models.ApplicationConfiguration(
     name="pipy3",
     application=os.path.join(ROOT_PATH, 'examples', 'src', 'main', 'python', 'pi.py'),
     application_args="30"
