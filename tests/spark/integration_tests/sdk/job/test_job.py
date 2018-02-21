52d44d25eaf5ca2894e2fc9e42ead22626111c60
Jake Freck <jafreck@microsoft.com>
2018-02-20 16:19:10 -0800

    fix job tests, issue with submit_job

--- tests/spark/integration_tests/sdk/job/test_job.py
+++ tests/spark/integration_tests/sdk/job/test_job.py
@@ -9,7 +9,7 @@ from cli import config
 
 dt = datetime.now()
 time = dt.microsecond
-base_job_id = "job-test{}".format(time)
+base_job_id = "job-{}".format(time)
 
 
 # load secrets
@@ -23,25 +23,24 @@ def test_submit_job():
         app1 = aztk.spark.models.ApplicationConfiguration(
             name="pipy100",
             application="examples/src/main/python/pi.py",
-            application_args="100"
+            application_args=[100]
         )
         app2 = aztk.spark.models.ApplicationConfiguration(
             name="pipy101",
             application="examples/src/main/python/pi.py",
-            application_args="101"
+            application_args=[100]
         )
         job_configuration = aztk.spark.models.JobConfiguration(
             id=test_id+base_job_id,
             applications=[app1, app2],
-            vm_size="standard_f2",
-            custom_scripts=None,
+            vm_size="standard_f1",
             spark_configuration=None,
             docker_repo=None,
             max_dedicated_nodes=2,
             max_low_pri_nodes=None
         )
         job = spark_client.submit_job(job_configuration=job_configuration)
-        spark_client.wait_until_job_finished(job_id=job.id)
+        spark_client.wait_until_job_finished(job_id=job_configuration.id)
     except (AztkError, BatchErrorException) as e:
         assert False
 
@@ -55,11 +54,20 @@ def test_list_jobs():
     test_id = "list-"
 
     try:
-
+        app1 = aztk.spark.models.ApplicationConfiguration(
+            name="pipy100",
+            application="examples/src/main/python/pi.py",
+            application_args=[100]
+        )
+        app2 = aztk.spark.models.ApplicationConfiguration(
+            name="pipy101",
+            application="examples/src/main/python/pi.py",
+            application_args=[100]
+        )
         job_configuration = aztk.spark.models.JobConfiguration(
             id=test_id+base_job_id,
-            applications=[],
-            vm_size="standard_f2",
+            applications=[app1, app2],
+            vm_size="standard_f1",
             custom_scripts=None,
             spark_configuration=None,
             docker_repo=None,
@@ -67,6 +75,7 @@ def test_list_jobs():
             max_low_pri_nodes=None
         )
         spark_client.submit_job(job_configuration=job_configuration)
+        spark_client.wait_until_job_finished(job_configuration.id)
 
         jobs = spark_client.list_jobs()
     except (AztkError, BatchErrorException) as e:
@@ -79,30 +88,31 @@ def test_list_jobs():
 
 
 def test_list_applications():
-    test_id = "list-applications-"
+    test_id = "list-apps-"
 
     try:
         app1 = aztk.spark.models.ApplicationConfiguration(
             name="pipy100",
             application="examples/src/main/python/pi.py",
-            application_args="100"
+            application_args=[100]
         )
         app2 = aztk.spark.models.ApplicationConfiguration(
             name="pipy101",
             application="examples/src/main/python/pi.py",
-            application_args="101"
+            application_args=[100]
         )
         job_configuration = aztk.spark.models.JobConfiguration(
             id=test_id+base_job_id,
             applications=[app1, app2],
-            vm_size="standard_f2",
+            vm_size="standard_f1",
             custom_scripts=None,
             spark_configuration=None,
             docker_repo=None,
             max_dedicated_nodes=2,
             max_low_pri_nodes=None
         )
-        job = spark_client.submit_job(job_configuration=job_configuration)
+        spark_client.submit_job(job_configuration=job_configuration)
+        spark_client.wait_until_job_finished(job_configuration.id)
 
         applications = spark_client.list_applications(job_id=job_configuration.id)
     except (AztkError, BatchErrorException) as e:
@@ -123,22 +133,22 @@ def test_get_job():
         app1 = aztk.spark.models.ApplicationConfiguration(
             name="pipy100",
             application="examples/src/main/python/pi.py",
-            application_args="100"
+            application_args=[100]
         )
         app2 = aztk.spark.models.ApplicationConfiguration(
             name="pipy101",
             application="examples/src/main/python/pi.py",
-            application_args="101"
+            application_args=[100]
         )
         job_configuration = aztk.spark.models.JobConfiguration(
             id=test_id+base_job_id,
             applications=[app1, app2],
-            vm_size="standard_f2",
+            vm_size="standard_f1",
             custom_scripts=None,
             spark_configuration=None,
             docker_repo=None,
-            max_dedicated_nodes=2,
-            max_low_pri_nodes=None
+            max_dedicated_nodes=None,
+            max_low_pri_nodes=1
         )
         spark_client.submit_job(job_configuration=job_configuration)
         spark_client.wait_until_job_finished(job_configuration.id)
@@ -155,18 +165,18 @@ def test_get_job():
 
 
 def test_get_application():
-    test_id = "get-application-"
+    test_id = "get-app-"
 
     try:
         app1 = aztk.spark.models.ApplicationConfiguration(
             name="pipy100",
             application="examples/src/main/python/pi.py",
-            application_args="100"
+            application_args=[100]
         )
         job_configuration = aztk.spark.models.JobConfiguration(
             id=test_id+base_job_id,
             applications=[app1],
-            vm_size="standard_f2",
+            vm_size="standard_f1",
             custom_scripts=None,
             spark_configuration=None,
             docker_repo=None,
@@ -189,18 +199,18 @@ def test_get_application():
 
 
 def test_get_application_log():
-    test_id = "get-application-log-"
+    test_id = "gal-"
 
     try:
         app1 = aztk.spark.models.ApplicationConfiguration(
             name="pipy100",
             application="examples/src/main/python/pi.py",
-            application_args="100"
+            application_args=[100]
         )
         job_configuration = aztk.spark.models.JobConfiguration(
             id=test_id+base_job_id,
             applications=[app1],
-            vm_size="standard_f2",
+            vm_size="standard_f1",
             custom_scripts=None,
             spark_configuration=None,
             docker_repo=None,
@@ -230,12 +240,12 @@ def test_delete_job():
         app1 = aztk.spark.models.ApplicationConfiguration(
             name="pipy100",
             application="examples/src/main/python/pi.py",
-            application_args="100"
+            application_args=[100]
         )
         job_configuration = aztk.spark.models.JobConfiguration(
             id=test_id+base_job_id,
             applications=[app1],
-            vm_size="standard_f2",
+            vm_size="standard_f1",
             custom_scripts=None,
             spark_configuration=None,
             docker_repo=None,
