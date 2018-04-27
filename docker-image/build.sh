1a8739ad3fe88d2e9f86fb52f2d162ceaa007c63
Jake Freck <jafreck@microsoft.com>
2018-04-27 15:03:16 -0700

    update build script to include spark1.6.3

--- docker-image/build.sh
+++ docker-image/build.sh
@@ -24,6 +24,10 @@ docker push aztk/spark:v0.1.0-spark2.2.0-base
 docker build base/spark2.3.0/ --tag aztk/spark:v0.1.0-spark2.3.0-base > out/base-spark2.3.0.out &&
 docker push aztk/spark:v0.1.0-spark2.3.0-base
 
+# miniconda-base 1.6.3
+docker build miniconda/spark1.6.3/base/ --tag aztk/spark:v0.1.0-spark1.6.3-miniconda-base > out/miniconda-spark1.6.3.out &&
+docker push aztk/spark:v0.1.0-spark1.6.3-miniconda-base
+
 # miniconda-base 2.1.0
 docker build miniconda/spark2.1.0/base/ --tag aztk/spark:v0.1.0-spark2.1.0-miniconda-base > out/miniconda-spark2.1.0.out &&
 docker push aztk/spark:v0.1.0-spark2.1.0-miniconda-base
@@ -36,6 +40,10 @@ docker push aztk/spark:v0.1.0-spark2.2.0-miniconda-base
 docker build miniconda/spark2.3.0/base/ --tag aztk/spark:v0.1.0-spark2.3.0-miniconda-base > out/miniconda-spark2.3.0.out &&
 docker push aztk/spark:v0.1.0-spark2.3.0-miniconda-base
 
+# anaconda-base 1.6.3
+docker build anaconda/spark1.6.3/base/ --tag aztk/spark:v0.1.0-spark1.6.3-anaconda-base > out/anaconda-spark1.6.3.out &&
+docker push aztk/spark:v0.1.0-spark1.6.3-anaconda-base
+
 # anaconda-base 2.1.0
 docker build anaconda/spark2.1.0/base/ --tag aztk/spark:v0.1.0-spark2.1.0-anaconda-base > out/anaconda-spark2.1.0.out &&
 docker push aztk/spark:v0.1.0-spark2.1.0-anaconda-base
@@ -47,6 +55,10 @@ docker push aztk/spark:v0.1.0-spark2.2.0-anaconda-base
 # anaconda-base 2.3.0
 docker build anaconda/spark2.3.0/base/ --tag aztk/spark:v0.1.0-spark2.3.0-anaconda-base > out/anaconda-spark2.3.0.out &&
 docker push aztk/spark:v0.1.0-spark2.3.0-anaconda-base
+
+# r-base 1.6.3
+docker build r/spark1.6.3/base/ --tag aztk/spark:v0.1.0-spark1.6.3-r-base > out/r-spark1.6.3.out &&
+docker push aztk/spark:v0.1.0-spark1.6.3-r-base
 
 # r-base 2.1.0
 docker build r/spark2.1.0/base/ --tag aztk/spark:v0.1.0-spark2.1.0-r-base > out/r-spark2.1.0.out &&
@@ -64,6 +76,10 @@ docker push aztk/spark:v0.1.0-spark2.3.0-r-base
 #      GPU       #
 ##################
 
+# gpu 1.6.3
+docker build gpu/spark1.6.3/ --tag aztk/spark:v0.1.0-spark1.6.3-gpu > out/gpu-spark1.6.3.out &&
+docker push aztk/spark:v0.1.0-spark1.6.3-gpu
+
 # gpu 2.1.0
 docker build gpu/spark2.1.0/ --tag aztk/spark:v0.1.0-spark2.1.0-gpu > out/gpu-spark2.1.0.out &&
 docker push aztk/spark:v0.1.0-spark2.1.0-gpu
@@ -75,6 +91,10 @@ docker push aztk/spark:v0.1.0-spark2.2.0-gpu
 # gpu 2.3.0
 docker build gpu/spark2.3.0/ --tag aztk/spark:v0.1.0-spark2.3.0-gpu > out/gpu-spark2.3.0.out &&
 docker push aztk/spark:v0.1.0-spark2.3.0-gpu
+
+# miniconda-gpu 1.6.3
+docker build miniconda/spark1.6.3/gpu/ --tag aztk/spark:v0.1.0-spark1.6.3-miniconda-gpu > out/miniconda-spark1.6.3.out &&
+docker push aztk/spark:v0.1.0-spark1.6.3-miniconda-gpu
 
 # miniconda-gpu 2.1.0
 docker build miniconda/spark2.1.0/gpu/ --tag aztk/spark:v0.1.0-spark2.1.0-miniconda-gpu > out/miniconda-spark2.1.0.out &&
@@ -88,6 +108,10 @@ docker push aztk/spark:v0.1.0-spark2.2.0-miniconda-gpu
 docker build miniconda/spark2.3.0/gpu/ --tag aztk/spark:v0.1.0-spark2.3.0-miniconda-gpu > out/miniconda-spark2.3.0.out &&
 docker push aztk/spark:v0.1.0-spark2.3.0-miniconda-gpu
 
+# anaconda-gpu 1.6.3
+docker build anaconda/spark1.6.3/gpu/ --tag aztk/spark:v0.1.0-spark1.6.3-anaconda-gpu > out/anaconda-spark1.6.3.out &&
+docker push aztk/spark:v0.1.0-spark1.6.3-anaconda-gpu
+
 # anaconda-gpu 2.1.0
 docker build anaconda/spark2.1.0/gpu/ --tag aztk/spark:v0.1.0-spark2.1.0-anaconda-gpu > out/anaconda-spark2.1.0.out &&
 docker push aztk/spark:v0.1.0-spark2.1.0-anaconda-gpu
@@ -99,6 +123,10 @@ docker push aztk/spark:v0.1.0-spark2.2.0-anaconda-gpu
 # anaconda-gpu 2.3.0
 docker build anaconda/spark2.3.0/gpu/ --tag aztk/spark:v0.1.0-spark2.3.0-anaconda-gpu > out/anaconda-spark2.3.0.out &&
 docker push aztk/spark:v0.1.0-spark2.3.0-anaconda-gpu
+
+# r-gpu 1.6.3
+docker build r/spark1.6.3/gpu/ --tag aztk/spark:v0.1.0-spark1.6.3-r-gpu > out/r-spark1.6.3.out &&
+docker push aztk/spark:v0.1.0-spark1.6.3-r-gpu
 
 # r-gpu 2.1.0
 docker build r/spark2.1.0/gpu/ --tag aztk/spark:v0.1.0-spark2.1.0-r-gpu > out/r-spark2.1.0.out &&
