ad1419806c1d4469b0c390d0d79aea2fdf09b5eb
Jake Freck <jafreck@microsoft.com>
2018-04-29 22:02:39 -0700

    add gpu support

--- aztk/spark/models/plugins/tensorflow_on_spark/tensorflow_on_spark.sh
+++ aztk/spark/models/plugins/tensorflow_on_spark/tensorflow_on_spark.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This plugin requires HDFS to be enabled and on the path
 
 # setup TensorFlowOnSpark
 git clone https://github.com/yahoo/TensorFlowOnSpark.git
@@ -7,5 +8,13 @@ cd TensorFlowOnSpark
 export TFoS_HOME=$(pwd)
 export TFoS_HOME=~/TensorFlowOnSpark >> ~/.bashrc
 
-pip install tensorflow-cpu
-pip install tensorflowonspark
+if  [ "$AZTK_GPU_ENABLED" = "true" ]; then
+    pip install tensorflow-gpu
+    pip install tensorflowonspark
+else
+    pip install tensorflow-cpu
+    pip install tensorflowonspark
+fi
+
+# add libhdfs.so to path
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HADOOP_HOME/lib/native/libhdfs.so" >> ~/.bashrc
