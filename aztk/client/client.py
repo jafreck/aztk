0da3f7342d64e9dd742929137ed2c621c70b32aa
Jake Freck <jafreck@microsoft.com>
2018-06-25 16:06:42 -0700

    fix imports

--- aztk/client/client.py
+++ aztk/client/client.py
@@ -1,5 +1,5 @@
-from aztk.spark.client.cluster import Client as cluster_client
-from aztk.spark.client.job import Client as job_client
+from aztk.client.cluster import Client as cluster_client
+from aztk.client.job import Client as job_client
 
 class Client:
     cluster = cluster_client
