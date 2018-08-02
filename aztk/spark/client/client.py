a27e3718497a91f6f0ca2f56d423f05f3ba415be
Jake Freck <jafreck@microsoft.com>
2018-08-02 16:54:43 -0700

    fix bug

--- aztk/spark/client/client.py
+++ aztk/spark/client/client.py
@@ -219,7 +219,7 @@ class Client(CoreClient):
     @deprecated("0.10.0")
     def wait_until_job_finished(self, job_id):
         try:
-            self.job.wait(self, job_id)
+            self.job.wait(job_id)
         except batch_error.BatchErrorException as e:
             raise error.AztkError(helpers.format_batch_exception(e))
 
