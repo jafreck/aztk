b1b17de682be6d150f6d3d0dcc391d292aa58c16
Jake Freck <jafreck@microsoft.com>
2018-08-06 15:55:54 -0700

    add brief flag

--- aztk/spark/client/cluster/helpers/diagnostics.py
+++ aztk/spark/client/cluster/helpers/diagnostics.py
@@ -6,37 +6,41 @@ from aztk import error
 from aztk.utils import helpers
 
 
-def _run(spark_cluster_operations, cluster_id, output_directory=None):
+def _run(spark_cluster_operations, cluster_id, output_directory=None, brief=False):
     # copy debug program to each node
     output = spark_cluster_operations.copy(
         cluster_id, os.path.abspath("./aztk/spark/utils/debug.py"), "/tmp/debug.py", host=True)
-    ssh_cmd = _build_diagnostic_ssh_command()
+    ssh_cmd = _build_diagnostic_ssh_command(brief)
     run_output = spark_cluster_operations.run(cluster_id, ssh_cmd, host=True)
     remote_path = "/tmp/debug.zip"
     if output_directory:
-        local_path = os.path.join(os.path.abspath(output_directory), "debug.zip")
-        output = spark_cluster_operations.download(cluster_id, remote_path, local_path, host=True)
-
+        output = spark_cluster_operations.download(cluster_id, remote_path, host=True)
+        os.makedirs(output_directory)
+        for node_output in output:
+            node_output.output.seek(0)
+            new_zip = os.path.join(output_directory, node_output.id + ".zip")
+            stream = open(new_zip, 'wb+')
+            stream.write(node_output.output.read())
         # write run output to debug/ directory
-        with open(os.path.join(os.path.dirname(local_path), "debug-output.txt"), 'w', encoding="UTF-8") as f:
-            [f.write(line + '\n') for node_output in run_output for line in node_output.output]
+        with open(os.path.join(output_directory, "debug-output.txt"), 'w', encoding="UTF-8") as f:
+            [f.write(node_output.output + '\n') for node_output in run_output]
     else:
         output = spark_cluster_operations.download(cluster_id, remote_path, host=True)
 
     return output
 
 
-def _build_diagnostic_ssh_command():
+def _build_diagnostic_ssh_command(brief):
     return "sudo rm -rf /tmp/debug.zip; "\
            "sudo apt-get install -y python3-pip; "\
            "sudo -H pip3 install --upgrade pip; "\
            "sudo -H pip3 install docker; "\
-           "sudo python3 /tmp/debug.py"
+           "sudo python3 /tmp/debug.py {}".format(brief)
 
 
-def run_cluster_diagnostics(spark_cluster_operations, cluster_id, output_directory=None):
+def run_cluster_diagnostics(spark_cluster_operations, cluster_id, output_directory=None, brief=False):
     try:
-        output = _run(spark_cluster_operations, cluster_id, output_directory)
+        output = _run(spark_cluster_operations, cluster_id, output_directory, brief)
         return output
     except batch_error.BatchErrorException as e:
         raise error.AztkError(helpers.format_batch_exception(e))
