82f8fdaf73295e1e1c659ba1e9266bfc95055bb1
Jake Freck <jafreck@microsoft.com>
2018-10-15 10:56:42 -0700

    fix node election exception catch

--- aztk/node_scripts/install/pick_master.py
+++ aztk/node_scripts/install/pick_master.py
@@ -3,7 +3,7 @@
 """
 import azure.batch.batch_service_client as batch
 import azure.batch.models as batchmodels
-import azure.batch.models.batch_error as batcherror
+from azure.batch.models.batch_error import BatchErrorException
 from msrest.exceptions import ClientRequestError
 
 from aztk.node_scripts.core import config
@@ -40,7 +40,7 @@ def try_assign_self_as_master(client: batch.BatchServiceClient, pool: batchmodel
             batchmodels.PoolPatchOptions(if_match=pool.e_tag),
         )
         return True
-    except (batcherror.BatchErrorException, ClientRequestError):
+    except (BatchErrorException, ClientRequestError):
         print("Couldn't assign itself as master the pool because the pool was modified since last get.")
         return False
 
