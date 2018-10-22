b139fff9c45515424e6ed720226a6711ce0945f0
Jake Freck <jafreck@microsoft.com>
2018-10-22 16:54:03 -0700

    remove unused imports

--- aztk/spark/client/client.py
+++ aztk/spark/client/client.py
@@ -1,16 +1,7 @@
-from typing import List
-
-from azure.batch.models import BatchErrorException
-
-from aztk import error
-from aztk import models as base_models
 from aztk.client import CoreClient
 from aztk.spark import models
 from aztk.spark.client.cluster import ClusterOperations
 from aztk.spark.client.job import JobOperations
-from aztk.spark.helpers import job_submission as job_submit_helper
-from aztk.spark.utils import util
-from aztk.utils import deprecate, deprecated, helpers
 
 
 class Client(CoreClient):
