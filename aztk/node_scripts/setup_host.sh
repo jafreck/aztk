d3efc866057e11ef7637bea69d1b75f3f659a9a3
Jake Freck <jafreck@microsoft.com>
2018-10-24 11:04:05 -0700

    retry docker pulls

--- aztk/node_scripts/setup_host.sh
+++ aztk/node_scripts/setup_host.sh
@@ -42,8 +42,8 @@ install_prerequisites () {
 
 install_docker_compose () {
     echo "Installing Docker-Compose"
-    for i in {1..5}; do 
-        sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && break || sleep 2; 
+    for i in {1..5}; do
+        sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && break || sleep 2;
     done
     sudo chmod +x /usr/local/bin/docker-compose
     echo "Finished installing Docker-Compose"
@@ -59,7 +59,12 @@ pull_docker_container () {
         docker login $DOCKER_ENDPOINT --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
     fi
 
-    docker pull $docker_repo_name
+
+    for i in {1..5}; do
+        docker pull $docker_repo_name && break ||
+        echo "ERROR: docker pull $docker_repo_name failed ... retrying after $($i**2) seconds" &&
+        sleep $i**2;
+    done
     echo "Finished pulling $docker_repo_name"
 }
 
