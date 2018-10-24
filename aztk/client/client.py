e665d19403042bc6d2ea1f43aff72875717f9804
Jake Freck <jafreck@microsoft.com>
2018-10-23 17:12:54 -0700

    remove unused imports

--- aztk/client/client.py
+++ aztk/client/client.py
@@ -1,20 +1,5 @@
-import asyncio
-import concurrent.futures
-from datetime import datetime, timedelta, timezone
-
-import azure.batch.models as batch_models
-from azure.batch.models import BatchErrorException
-from Cryptodome.PublicKey import RSA
-
-import aztk.error as error
-import aztk.models as models
-import aztk.utils.azure_api as azure_api
-import aztk.utils.constants as constants
-import aztk.utils.get_ssh_key as get_ssh_key
-import aztk.utils.helpers as helpers
-import aztk.utils.ssh as ssh_lib
-from aztk.internal import cluster_data
-from aztk.utils import deprecated, secure_utils
+from aztk import models
+from aztk.utils import azure_api
 
 
 class CoreClient:
@@ -29,6 +14,7 @@ class CoreClient:
         self.secrets_configuration = None
         self.batch_client = None
         self.blob_client = None
+        self.table_service = None
 
     def _get_context(self, secrets_configuration: models.SecretsConfiguration):
         self.secrets_configuration = secrets_configuration
